3fee99c2b09ca657106ca74c17c7b8f02a1a3bc2
Nouah <noahgreatakoni@gmail.com>
2022-11-05 22:14:18 +0100

    basic-auth

---
+++ src/main/java/authentication/com/springsecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,61 @@
+package authentication.com.springsecurity.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static authentication.com.springsecurity.security.ApplicationUserRole.ADMIN;
+import static authentication.com.springsecurity.security.ApplicationUserRole.STUDENT;
+
+@Configuration
+@EnableWebSecurity
+
+//basic authentication doesn't permit a user to log out after signing in
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","index","/css/*","/js/*") .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails jones = User.builder()
+                .username("williams")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name())
+                .build();
+
+        UserDetails eva = User
+                .builder()
+                .username("Eva")
+                .password(passwordEncoder.encode("password444"))
+                .roles(ADMIN.name())
+
+                .build();
+
+return new InMemoryUserDetailsManager(jones,eva);
+
+    }
+}
