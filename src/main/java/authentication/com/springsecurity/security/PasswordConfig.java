eab7a1c2c8ec20f8957f4f535b42beaa2748caba
Nouah <noahgreatakoni@gmail.com>
2022-11-05 22:16:32 +0100

    bcrypt-auth

---
+++ src/main/java/authentication/com/springsecurity/security/PasswordConfig.java
@@ -0,0 +1,15 @@
+package authentication.com.springsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+//bcrypt authentication
+public class PasswordConfig {
+@Bean
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder(10);
+    }
+}
