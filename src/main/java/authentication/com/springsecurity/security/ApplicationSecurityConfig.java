96d6745521cb051fc2dbc53744905d360ab5afca
Nouah <noahgreatakoni@gmail.com>
2022-11-09 05:57:33 +0100

    role-based

--- src/main/java/authentication/com/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/authentication/com/springsecurity/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package authentication.com.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,12 +14,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static authentication.com.springsecurity.security.ApplicationUserRole.ADMIN;
-import static authentication.com.springsecurity.security.ApplicationUserRole.STUDENT;
+import static authentication.com.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
+import static authentication.com.springsecurity.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
-
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 //basic authentication doesn't permit a user to log out after signing in
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder passwordEncoder;
@@ -30,9 +32,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
+                .csrf().disable()
+                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*") .permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                //permission based auth
+                //the order at which  the ant-matchers are arranged does matter
+              /*  .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name()) */
+
+
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,18 +56,28 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails jones = User.builder()
                 .username("williams")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
+               // .roles(STUDENT.name())
                 .build();
 
         UserDetails eva = User
                 .builder()
                 .username("Eva")
                 .password(passwordEncoder.encode("password444"))
-                .roles(ADMIN.name())
-
+              //  .roles(ADMINTRAINEE.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
-return new InMemoryUserDetailsManager(jones,eva);
+        UserDetails tom = User
+                .builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+      //          .roles(ADMIN.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+
+
+        return new InMemoryUserDetailsManager(jones,eva,tom);
 
     }
 }
