bb6ca17d0fe6a7ba4782bf33f7dd4b2ef1f8b496
Nouah <noahgreatakoni@gmail.com>
2022-11-12 14:28:07 +0100

    role-based

--- src/main/java/authentication/com/springsecurity/controller/StudentController.java
+++ src/main/java/authentication/com/springsecurity/controller/StudentController.java
@@ -2,6 +2,7 @@ package authentication.com.springsecurity.controller;
 
 import authentication.com.springsecurity.data.model.Student;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
