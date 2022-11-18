3fe64bf9976fc87752614e4dd2de51d2be1e99fe
Nouah <noahgreatakoni@gmail.com>
2022-11-18 16:01:25 +0100

    log-out added

--- src/main/java/authentication/com/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/authentication/com/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,9 +1,11 @@
 package authentication.com.springsecurity.security;
 
+import authentication.com.springsecurity.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,8 +15,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import static authentication.com.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
+import java.util.concurrent.TimeUnit;
+
 import static authentication.com.springsecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -22,62 +26,57 @@ import static authentication.com.springsecurity.security.ApplicationUserRole.*;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 //basic authentication doesn't permit a user to log out after signing in
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final PasswordEncoder passwordEncoder;
-
+ /*  private final PasswordEncoder passwordEncoder;
+private final ApplicationUserService  applicationUserService;
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
-
+*/
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+              //  .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .csrf().disable()
-                 .authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*") .permitAll()
+//csrf:Cross Site Request Forgery, an action of forging a copy or imitation of a document,signature,banknote
+                //or work of art
+                //it is advised to use CSRF  protection for any request that could be processed by a browser by normal users
+                //and not by  a non-browser
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 //permission based auth
                 //the order at which  the ant-matchers are arranged does matter
-              /*  .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name()) */
-
-
+                /*  .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                  .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                  .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                  .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name()) */
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                //basic auth
+      //  .httpBasic();
+        //for form-based auth
+               .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses",true)
+                .passwordParameter("password")
+                .usernameParameter("username")
+                .and()
+                .rememberMe()
+                //overriding 2weeks validity code
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)).key("very secured")
+                .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID","remember-me")
+                .logoutSuccessUrl("/login");
     }
 
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails jones = User.builder()
-                .username("williams")
-                .password(passwordEncoder.encode("password"))
-                .authorities(STUDENT.getGrantedAuthorities())
-               // .roles(STUDENT.name())
-                .build();
 
-        UserDetails eva = User
-                .builder()
-                .username("Eva")
-                .password(passwordEncoder.encode("password444"))
-              //  .roles(ADMINTRAINEE.name())
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-        UserDetails tom = User
-                .builder()
-                .username("tom")
-                .password(passwordEncoder.encode("password123"))
-      //          .roles(ADMIN.name())
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-
-        return new InMemoryUserDetailsManager(jones,eva,tom);
-
-    }
 }
